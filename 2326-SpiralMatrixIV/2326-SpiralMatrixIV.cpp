a55888107212ee51562210149426fce7ef6ffbe3
Chitresh Gyanani <129378666+Chitresh-code@users.noreply.github.com>
2024-09-09 15:22:43 +0530

    Create file

---
+++ 2326-SpiralMatrixIV/2326-SpiralMatrixIV.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
+        vector<vector<int>> matrix(m, vector<int>(n, -1));
+        
+        int top = 0, bottom = m - 1, left = 0, right = n - 1;
+        
+        ListNode* current = head;
+        
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right && current != nullptr; ++i) {
+                matrix[top][i] = current->val;
+                current = current->next;
+            }
+            ++top;
+            
+            for (int i = top; i <= bottom && current != nullptr; ++i) {
+                matrix[i][right] = current->val;
+                current = current->next;
+            }
+            --right;
+            
+            for (int i = right; i >= left && current != nullptr; --i) {
+                matrix[bottom][i] = current->val;
+                current = current->next;
+            }
+            --bottom;
+            
+            for (int i = bottom; i >= top && current != nullptr; --i) {
+                matrix[i][left] = current->val;
+                current = current->next;
+            }
+            ++left;
+        }
+        
+        return matrix;
+    }
+};
