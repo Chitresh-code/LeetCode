2a6ea6ab20b0f74334ad13923928391d68b58102
Chitresh Gyanani <129378666+Chitresh-code@users.noreply.github.com>
2024-11-08 10:02:35 +0530

    Create file

---
+++ 1829-MaximumXORforEachQuery/1829-MaximumXORforEachQuery.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+   vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+       vector<int> answer(nums.size());
+       int mask = (1 << maximumBit) - 1, xr = 0;
+       for (int i = 0; i < nums.size(); ++i) {
+           xr ^= nums[i];
+           answer[nums.size() - 1 - i] = (~xr) & mask;
+       }
+       return answer;
+   }
+};
